23f1c1b0b7d5069db7064b60222d03eb294c9b59
Evert van Nieuwenburg <evert.v.nieuwenburg@gmail.com>
2020-12-20 15:31:30 +0100

    Add ToricGame

--- scigym/envs/__init__.py
+++ scigym/envs/__init__.py
@@ -1,6 +1,5 @@
 from gym.envs.registration import registry, register, make, spec
 
-
 register(
     id='surfacecode-decoding-v0',
     entry_point='scigym.envs.quantum_physics.quantum_computing.surfacecode_decoding:SurfaceCodeEnv',
@@ -10,5 +9,11 @@ register(
 register(
     id='teleportation-v0',
     entry_point="scigym.envs.quantum_physics.quantum_computing.teleportation:TeleportationEnv",
+    nondeterministic=True
+)
+
+register(
+    id='toricgame-v0',
+    entry_point="scigym.envs.quantum_physics.quantum_computing.toricgame:ToricGameEnv",
     nondeterministic=True
 )
